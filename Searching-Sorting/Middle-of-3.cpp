615b43ab07e500eaa9589d34eaacbe0871399bce
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-01-26 15:47:03 +0530

    Create Middle-of-3.cpp

---
+++ Searching-Sorting/Middle-of-3.cpp
@@ -0,0 +1,15 @@
+class Solution{
+  public:
+    int middle(int A, int B, int C){
+        //code here//Position this line where user code will be pasted.
+        if(A>B && A<C || A<B && A>C){
+            return A;
+        }
+        else if(B>A && B<C || B>C && B<A){
+            return B;
+        }
+        else{
+            return C;
+        }
+    }
+};
